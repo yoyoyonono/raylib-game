4e5250585358f9c2a3c9bbb9e87c4e1292ad35b7
Aaron Thapa <notsmart98@gmail.com>
2024-06-27 16:49:48 +0545

    physics

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,19 @@ use raylib::prelude::*;
 enum GameScreen {
     Logo,
     Title,
-    Gameplay,
-    Ending
+    Gameplay
+}
+
+struct Player {
+    x: f32,
+    y: f32,
+    dx: f32,
+    dy: f32
 }
 
 fn main() {
-    let screen_width = 800;
-    let screen_height = 450;
+    let screen_width = 1280;
+    let screen_height = 720;
 
     let (mut rl, thread) = raylib::init()
         .size(screen_width, screen_height)
@@ -21,6 +27,8 @@ fn main() {
     let mut current_screen = GameScreen::Logo;
 
     let mut frame_counter = 0;
+
+    let mut player = Player {x: (screen_width/2) as f32, y: screen_height as f32, dx: 0.0, dy: 0.0};
 
     while !rl.window_should_close() {
         // Update
@@ -38,13 +46,26 @@ fn main() {
                 }
             }
             GameScreen::Gameplay => {
-                if rl.is_key_pressed(KeyboardKey::KEY_ENTER) {
-                    current_screen = GameScreen::Ending;
+                player.dx = 0.0;
+                player.dy += 0.1;
+                if player.y >= screen_height as f32 {
+                    player.dy = 0.0;
                 }
-            }
-            GameScreen::Ending => {
-                if rl.is_key_pressed(KeyboardKey::KEY_ENTER) {
-                    current_screen = GameScreen::Title;
+                if rl.is_key_down(KeyboardKey::KEY_A) {
+                    player.dx -= 5.0;
+                }
+                if rl.is_key_down(KeyboardKey::KEY_D) {
+                    player.dx += 5.0;
+                }
+                if rl.is_key_pressed(KeyboardKey::KEY_W) {
+                    player.dy = -5.0;
+                }
+
+                player.x += player.dx;
+                player.y += player.dy;
+
+                if player.y >= screen_height as f32 {
+                    player.y = screen_height as f32;
                 }
             }
         }
@@ -52,27 +73,25 @@ fn main() {
         // Draw
 
         let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::WHITE);
 
         match current_screen {
             GameScreen::Logo => {
-                d.draw_text("LOGO SCREEN", 20, 20, 40, Color::LIGHTGRAY);
-                d.draw_text("WAIT for 2 SECONDS...", 290, 220, 20, Color::GRAY);
+                d.draw_text("Logo", screen_width/2, screen_height/2, 40, Color::LIGHTGRAY);
+                d.draw_text("Copyright notice", screen_width/2, screen_height*3/4, 20, Color::GRAY);
             }
             GameScreen::Title => {
-                d.draw_rectangle(0, 0, screen_width, screen_height, Color::GREEN);
-                d.draw_text("TITLE SCREEN", 20, 20, 40, Color::DARKGREEN);
-                d.draw_text("PRESS ENTER to JUMP to GAMEPLAY SCREEN", 130, 220, 20, Color::DARKGREEN);
+                d.draw_text("TITLE SCREEN", 20, 20, 40, Color::LIGHTGRAY);
+                d.draw_text("press enter to start", 130, 220, 20, Color::GRAY);
             }
             GameScreen::Gameplay => {
-                d.draw_rectangle(0, 0, screen_width, screen_height, Color::PURPLE);
                 d.draw_text("GAMEPLAY SCREEN", 20, 20, 40, Color::MAROON);
-                d.draw_text("PRESS ENTER to JUMP to ENDINGS SCREEN", 130, 220, 20, Color::MAROON);
-            }
-            GameScreen::Ending => {
-                d.draw_rectangle(0, 0, screen_width, screen_height, Color::BLUE);
-                d.draw_text("ENDING SCREEN", 20, 20, 40, Color::DARKBLUE);
-                d.draw_text("PRESS ENTER to RETURN to TITLE SCREEN", 120, 220, 20, Color::DARKBLUE);
+                draw_player(&mut d, &player);
             }
         }
     }
+}
+
+fn draw_player(d: &mut RaylibDrawHandle, player: &Player) {
+    d.draw_rectangle(player.x as i32 - 20, player.y as i32 - 40, 40, 40, Color::RED);
 }
